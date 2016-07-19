698f22307d5444442bfbf0fabc1e9694e90568a3
Erika L. McLean <elmclean@lan19-58.svsu.edu>
2016-07-19 15:27:35 -0400

    checking for empty values

--- app/views/race.blade.php
+++ app/views/race.blade.php
@@ -10,12 +10,17 @@
 
 <div class="row">
 	<div class="col-xs-12">
-		{{ HTML::image('images/' . strtolower($race->race_name) . '.jpg', '', array('class' => 'col-xs-3 img-responsive')) }}
-		<dl class="dl-horizontal col-xs-9">
+		{{ HTML::image('images/' . strtolower($race->race_name) . '.jpg', '', array('class' => 'col-xs-12 col-md-5 col-lg-4 img-responsive')) }}
+		<dl class="dl-horizontal col-xs-12 col-md-7 col-lg-8">
 	        @foreach($race->getAttributes() as $attr => $value)
-	        	@if($attr != 'id')
-		        	<dt>{{ ucfirst(str_replace('_', ' ', $attr)) }}</dt>
-		        	<dd>{{ $value }}</dd>
+	        	@if($attr != 'id' && $attr != 'race_description')
+	        		@if(!empty($value))
+			        	<dt>{{ ucfirst(str_replace('_', ' ', $attr)) }}</dt>
+			        	<dd>{{ $value }}</dd>
+		        	@else
+		        		<dt>{{ ucfirst(str_replace('_', ' ', $attr)) }}</dt>
+		        		<dd>N/A</dd>
+		        	@endif
 	        	@endif
 	        @endforeach
 	    </dl>
